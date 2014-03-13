7f34ef9e6d6ca7b87a71b0648854985db44882d6
300assembler <300assembler@gmail.com>
2014-03-13 16:48:46 -0400

    Create LabelTable.cpp

---
+++ LabelTable.cpp
@@ -0,0 +1,7 @@
+//This is a data structure that holds labels and their corresponding addresses.
+//When a label is found, it can be added to this along with the address it was found at.
+//Later, a label is used, it can be used as a key to return that value.
+
+class LabelTable{
+
+};
