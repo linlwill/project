a2617e44557768882764e47cca2d1cdc846b6593
300assembler <300assembler@gmail.com>
2014-03-13 17:28:46 -0400

    Create outline.cpp

---
+++ outline.cpp
@@ -0,0 +1,7 @@
+//This is a highly-abstracted but hypothetically viable outline of how the assembler will function.  It is almost certainly full of errors, but w/e
+#include <fstream>
+
+int main(int first, char** input){
+  //input will be the text given when calling this function from the command line
+  
+}
