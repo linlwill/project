ebf7e667e01e6025105f073fcd57fc1fbd240af8
300assembler <300assembler@gmail.com>
2014-03-15 02:53:00 -0400

    Overhauled it to fix bugs

--- Base.h
+++ Base.h
@@ -1,61 +1,72 @@
-#ifndef BASEBLOCKS_INCLUDED
-#define BASEBLOCKS_INCLUDED
+#ifndef BASE_INCLUDED
+#define BASE_INCLUDED
 #include "LinkedList.h"
 //Provide functions to begin and end base blocks, then later check if where they are is in a block and if so, what that block's value is
+namespace Base {
+
 class BaseBlock{
   public:
   int startAddress;
   int value;
   int endAddress;
-  
-  BaseBlock(int a, int b){
+
+  BaseBlock(){}
+
+  BaseBlock(int a, int b, int c){
     startAddress = a;
     value = b;
+    endAddress = c;
   }//end constructor
 };//end class.  Would have been struct but class made LinkedList utilization easier.
 
 bool openBaseBlock = false;
 LinkedList<BaseBlock> BaseBlockList = LinkedList<BaseBlock>();
 
-void startBaseBlock(int address, int value);
-void endBaseBlock(int address);
-bool inBaseBlock(int address);
+void startBlock(int address, int value);
+void endBlock(int address);
+bool inBlock(int address);
 int getBase(int address);
+int buffer[2];
 
-void startBaseBlock(int address, int value){
+void startBlock(int address, int value){
   //iff there isn't a block open already, open one with corresponding values.
   if (!openBaseBlock){
     openBaseBlock = true;
-    BaseBlockList.add(BaseBlock(address,value));
+    buffer[0] = address;
+    buffer[1] = value;
   }//end if
 }//end startblock
 
-void endBaseBlock(int address){
+void endBlock(int address){
   //iff there is an open block, grab to the top of the list and set its end to end.  Then close the block.
   if (openBaseBlock){
     openBaseBlock = false;
-    (*BaseBlockList.top).content.endAddress = address;
+    BaseBlockList.add(BaseBlock(buffer[0],buffer[1],address));
   }//end if
 }//end endblock
 
-bool inBaseBlock(int address){
+bool inBlock(int address){
   //O(N) search of the base blocks checking if address is in bounds
-  BaseBlock temp;
+  BaseBlock bTemp = BaseBlock();
+  BaseBlock& temp = bTemp;
   for (int i = 0; i < BaseBlockList.getLength(); i++){
     temp = BaseBlockList[i];
-    if ((address >= temp.beginAddress)&&(address <= temp.endAddress)) return true;
+    //if ((address >= (*temp).startAddress)&&(address <= (*temp).endAddress)) return true;
+    if ((address >= temp.startAddress)&&(address <= temp.endAddress)) return true;
   }//end for
   return false;
 }//end inblock
 
 int getBase(int address){
   //Another search just like inblock.  This time return value.  Return -1 on not found, so this could be used as bool too.
-  BaseBlock temp;
+  BaseBlock bTemp = BaseBlock();
+  BaseBlock& temp = bTemp;
   for (int i = 0; i < BaseBlockList.getLength(); i++){
     temp = BaseBlockList[i];
-    if ((address >= temp.beginAddress)&&(address <= temp.endAddress)) return temp.value;
+    if ((address >= temp.startAddress)&&(address <= temp.endAddress)) return temp.value;
   }//end for
   return -1;
 }//end getBase
 
+}//end namespace
 #endif
