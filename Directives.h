3272d8c306c4d625d2209ff3617bfdd37698828d
300assembler <300assembler@gmail.com>
2014-03-26 12:21:32 -0400

    Update Directives.h

--- Directives.h
+++ Directives.h
@@ -1,15 +1,24 @@
 #ifndef DIRECTIVES_INCLUDED
 #define DIRECTIVES_INCLUDED
 #include <string>
+#include <BinarySearchTree.h>
 
-std::string directivesDB[?] = {
+namespace directives {
+//Keep a BST of all directive names.
+BinarySearchTree<std::string> DB = BinarySearchTree<std::string>();
+bool DBinitialized = false;
+void initDB(){
+  if (DBinitialized) return;
+  directives::DBinitialized = true;
+  DB.add("START");
+  DB.add("END");
+  DB.add("BYTE");
+  DB.add("RESB");
+  DB.add("WORD");
+  DB.add("RESW");
+  DB.add("BASE");
+  DB.add("EQU");
+}//end init
 
-  };//end array
-
-bool isDirective(std::string keyword){
-  for (int i = 0; i < ?; i++){
-    if (directivesDB[i] == keyword) return true;
-  }//end for
-  return false;
-}//end get
+}//end namespace
 #endif
