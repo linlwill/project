066c752360c7ce2cc2256a16b041926974f4dde1
300assembler <300assembler@gmail.com>
2014-03-26 12:39:26 -0400

    Used a switch instead of if-else

--- ObjectCodeGenerator.h
+++ ObjectCodeGenerator.h
@@ -43,57 +43,56 @@ std::string objectCode(std::string lineOfCode, int currentAddress = 0){
     Instruction theInst = getInstruction(opor);
     if ( (e == '1') && (theInst.format != 3) ) return "!!! ERROR: EXTENDED FORMAT USED IN NON-EXTENDABLE INSTRUCTION !!!";
 
-    if (theInst.format == 1){
-        return theInst.opcode.getHex(2);
-    }//end mode 1 - easy mode
-
-    else if (theInst.format == 2){
-        Reg::initDB();
-        objCode += theInst.opcode.getHex(2);
-        //Operand will be the two registers to act on demarked by ,
-        LinkedList<std::string> registers = divideString(opand,',');
-        if (registers.getLength() != 2) return "!!! ERROR: INVALID OPERAND FOR TYPE-2 INSTRUCTION !!!";
-        std::string R1 = registers[0];
-        std::string R2 = registers[1];
-        //Add each register's value in hex to the string.
-        if ( (!Reg::DB.contains(R1)) && (!Reg::DB.contains(R2)) ) return "!!! ERROR: UNRECOGNIZED REGISTER IN MODE2 OBJECT CODE !!!";
-        objCode += Hex(Reg::DB[R1]).getHex();
-        objCode += Hex(Reg::DB[R2]).getHex();
-    }//end mode 2 - Medium mode
-
-    else if (theInst.format == 3){
-        //Address is what we WANT, not what we NEED.  However, flagbits' bp() converts them silently.
-        std::string nix = fb::nix(&opand);
-        std::string bp;
-
-        int address;
-        //Convert operand into a numerical address.  What we "want".  If it's a number, just conver it to an int as-is.
-        if ((opand[0] >= '0') && (opand[0] <= '9')) std::stringstream(opand) >> address;
-        else if (LabelTable.contains(opand)) address = LabelTable[opand];
-        else return "!!! ERROR: UNRECOGNIZED LABEL IN MODE3 OBJECT CODE !!!";
-
-        //If we're in I or E, we don't need b or p.  Otherwise, we do.
-        if ((nix[1]-'0')||(e-'0')) bp = "00";
-        else bp = fb::bp(&address,currentAddress);
-
-        //nixbpe have been set, and opor and opand have been modified accordingly.  Assemble the string.
-        Hex opcode = theInst.opcode;
-        std::string bin = "B";
-        bin+= nix[0];
-        bin += nix[1];
-        opcode += bin;
-
-        bin = "B";
-        bin += nix[2];
-        bin += bp;
-        bin += e;
-        Hex xbpe = Hex(bin);
-
-        int len = 3 + (e - '0')*2;
-        std::string finalAddr = Hex(address).getHex(len);
-
-        objCode += (opcode.getHex(2)+xbpe.getHex()+finalAddr);
-    }//end mode 3 - Hard mode
+    switch(theInst.format){
+        case 1:
+            objCode = theInst.opcode.getHex(2);
+        case 2:
+            Reg::initDB();
+            objCode += theInst.opcode.getHex(2);
+            //Operand will be the two registers to act on demarked by ,
+            LinkedList<std::string> registers = divideString(opand,',');
+            if (registers.getLength() != 2) return "!!! ERROR: INVALID OPERAND FOR TYPE-2 INSTRUCTION !!!";
+            std::string R1 = registers[0];
+            std::string R2 = registers[1];
+            //Add each register's value in hex to the string.
+            if ( (!Reg::DB.contains(R1)) && (!Reg::DB.contains(R2)) ) return "!!! ERROR: UNRECOGNIZED REGISTER IN MODE2 OBJECT CODE !!!";
+            objCode += Hex(Reg::DB[R1]).getHex();
+            objCode += Hex(Reg::DB[R2]).getHex();
+        case 3:
+            //Address is what we WANT, not what we NEED.  However, flagbits' bp() converts them silently.
+            std::string nix = fb::nix(&opand);
+            std::string bp;
+    
+            int address;
+            //Convert operand into a numerical address.  What we "want".  If it's a number, just conver it to an int as-is.
+            if ((opand[0] >= '0') && (opand[0] <= '9')) std::stringstream(opand) >> address;
+            else if (LabelTable.contains(opand)) address = LabelTable[opand];
+            else return "!!! ERROR: UNRECOGNIZED LABEL IN MODE3 OBJECT CODE !!!";
+    
+            //If we're in I or E, we don't need b or p.  Otherwise, we do.
+            if ((nix[1]-'0')||(e-'0')) bp = "00";
+            else bp = fb::bp(&address,currentAddress);
+    
+            //nixbpe have been set, and opor and opand have been modified accordingly.  Assemble the string.
+            Hex opcode = theInst.opcode;
+            std::string bin = "B";
+            bin+= nix[0];
+            bin += nix[1];
+            opcode += bin;
+    
+            bin = "B";
+            bin += nix[2];
+            bin += bp;
+            bin += e;
+            Hex xbpe = Hex(bin);
+    
+            int len = 3 + (e - '0')*2;
+            std::string finalAddr = Hex(address).getHex(len);
+    
+            objCode += (opcode.getHex(2)+xbpe.getHex()+finalAddr);
+        default:
+            break;//Should never fire
+    }//end switch
 
     return objCode;
 }
