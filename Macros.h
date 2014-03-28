40fda40cb5dff307299c636762b9b80aac72877f
300assembler <300assembler@gmail.com>
2014-03-27 21:50:38 -0400

    Update Macros.h

--- Macros.h
+++ Macros.h
@@ -1,4 +1,14 @@
 #ifndef MACROSINCLUDED
 #define MACROSINCLUDED
+#include <map>
+#include <string>
 
+namespace macros {
+  
+class Macro;
+map<std::string,int> DB;
+void initDB();
+Macro get(std::string keyword);
+
+}//end namespace
 #endif
