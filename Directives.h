aafa808b544314355989c2e1ffe8e32c153d586b
300assembler <300assembler@gmail.com>
2014-03-27 21:48:29 -0400

    Update Directives.h

--- Directives.h
+++ Directives.h
@@ -6,6 +6,9 @@
 namespace directives {
   //Keyword will map to an integer referring to 1+ how many arguments the directive takes.  1+ is necessary so 0 can be "does not include" instead of "no arguments"
   map<std::string,int> DB;
+  void initDB();
+  int get(std::string keyword);
+  void process(std::string opor, std::string* opands);
 
 
 }//end namespace
