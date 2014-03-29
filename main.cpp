a811bb1173f3a93fa3c1dbc8a368f49f2d7fdb6d
300assembler <300assembler@gmail.com>
2014-03-29 12:54:03 -0400

    Fixed an issue with directive arguments
    The error catch would have done bad things.

--- main.cpp
+++ main.cpp
@@ -33,17 +33,20 @@ for (line in file){
       blockList = divideString(line,' ');
       opor = blockList[label];
       
-      argCount = directives::get(opor)-1+label;
+      //0 will be label, or empty.  1... will be arguments, the amount of which is dictated by the directive.  Get thus dictates how many things to grab, b/c the extra 1 that exists so 1 can be 0 and 0 can be n/a compensates for the label.  I love when stuff synergizes.
+      argCount = directives::get(opor);
       args = new std::string[argCount];
       if (label) args[0] = blockList[0];
       //For nonzero arguments, go one by one through the directive's demanded operands.  It's possible to run out of blocks here, like BASE asking for one but allowing none, so use LinkedList's exception handling.
       try{
-        for (int i = 1; i <= argCount; i++) args[i] = blockList[label+i];
+         for (int i = 1; i < argCount; i++) args[i] = blockCount[i];
       } catch(NotInListException){
-        //Cricket sounds
+         //Cricket sounds.  new[] initializes the spaces to "", so passing as-is preserves empties as flags.
       }//end exceptions
       directives::process(opor,args);
       delete[] args;
+    case 5://Macro.  Start grabbing lines.
+        //Not implemented yet!
     case 0://Exception.  NBD here.
       continue;
   }//end switch
