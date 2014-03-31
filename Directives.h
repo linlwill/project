e858f65f90a38b584704c758632764077aeac006
300assembler <300assembler@gmail.com>
2014-03-31 16:44:33 -0400

    Update Directives.h

--- Directives.h
+++ Directives.h
@@ -1,8 +1,17 @@
 #ifndef DIRECTIVES_INCLUDED
 #define DIRECTIVES_INCLUDED
 #include <string>
-#include <sstream>
+#include <math.h>
 #include <map>
+
+int toInt(std::string a){
+    //For every digit, handle as a number using base-10
+    int sum = 0;
+    int neg = 0;
+    if (a[0] == '-') neg = 1;
+    for (int i = neg; i < a.length(); i++) sum += pow(10, a[i-neg] - '0');
+    return sum * pow(-1,neg);
+}//end toInt
 
 namespace directives {
   //Keyword will map to an integer referring to 1+ how many arguments the directive takes.  1+ is necessary so 0 can be "does not include" instead of "no arguments"
@@ -37,16 +46,14 @@ namespace directives {
     
     else if (opor == "EQU"){
       //Create a constant, with value label.  Convert string to int and set as map value.
-      int value;
-      std::stringstream(opands[1]) >> value;
-      labelTable[opands[0]] = value;
+      labelTable[opands[0]] = toInt(opands[1]);
     }//end equ
     
     else if (opor == "START"){
       //The operand is where to start the program, as an integer.  Later this function can be expanded to handle control sections/program blocks.
-      int value;
-      std::stringstream(opands[1]) >> value;
-      primary::CurrentAddress = value;
+      int a = toInt(opands[1]);
+      primary::CurrentAddress = a;
+      primary::startingAddress = a;
     }//end start
     
   }//end process
