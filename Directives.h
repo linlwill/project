810c430c912da85ce08d06caf4b0e1e0bccb5337
300assembler <300assembler@gmail.com>
2014-04-01 11:11:58 -0400

    Update Directives.h

--- Directives.h
+++ Directives.h
@@ -1,16 +1,8 @@
 #ifndef DIRECTIVES_INCLUDED
 #define DIRECTIVES_INCLUDED
-#include <math.h>
+#include <sstream>
 #include "primary.h"
 
-int toInt(std::string a){
-    //For every digit, handle as a number using base-10
-    int sum = 0;
-    int neg = 0;
-    if (a[0] == '-') neg = 1;
-    for (int i = neg; i < a.length(); i++) sum += pow(10, a[i-neg] - '0');
-    return sum * pow(-1,neg);
-}//end toInt
 
 namespace directives {
   //Keyword will map to an integer referring to 1+ how many arguments the directive takes.  1+ is necessary so 0 can be "does not include" instead of "no arguments"
@@ -37,20 +29,24 @@ namespace directives {
     //Wish I could use switches with strings.  I like switches.
     if (opor == "BASE"){
       //Set the base to be the operand.  Or call empty to close any bases. 
-      std::string opand = opands[1];
       //There is no harm in calling close even if there isn't one open.
       Base::endBlock(primary::CurrentAddress);
-      if (opand != "") Base::startBlock(toInt(opand),primary::CurrentAddress);
+      int i;
+      std::stringstream(opands[1]) >> i;
+      if (opands[1] != "") Base::startBlock(i,primary::CurrentAddress);
     }//end base
     
     else if (opor == "EQU"){
       //Create a constant, with value label.  Convert string to int and set as map value.
-      primary::labelTable[opands[0]] = toInt(opands[1]);
+      int i;
+      std::stringstream(opands[1]) >> i;
+      primary::labelTable[opands[0]] = i;
     }//end equ
     
     else if (opor == "START"){
       //The operand is where to start the program, as an integer.  Later this function can be expanded to handle control sections/program blocks.
-      int a = toInt(opands[1]);
+      int a;
+      std::stringstream([1]) >> a;
       primary::CurrentAddress = a;
       primary::startingAddress = a;
     }//end start
