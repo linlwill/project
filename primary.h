2727dae810ed46e2a9467c83c4f47185ee4a5271
300assembler <300assembler@gmail.com>
2014-04-02 13:00:50 -0400

    Update primary.h

--- primary.h
+++ primary.h
@@ -14,8 +14,9 @@ namespace primary{
     int forceInt(std::string input){
         //Evaluate based on integer, hexidecimal, binary, {char, or arithmatic} input.
         std::string id = input.substr(0,2);
-        if (id == "X'") return Hex.toInt(input.substr(2,input.length()-1));
-        else if (id == "B'") return Hex.toInt("B"+input.substr(2,input.length()-1));
+        int end = 1;//(input[length()-1] == 39) ? 1:0;
+        if (id == "X'") return Hex.toInt(input.substr(2,input.length()-end));
+        else if (id == "B'") return Hex.toInt("B"+input.substr(2,input.length()-end));
         else return Hex.toInt("I"+input);
     }//end force-int
 }
